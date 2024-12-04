0d87c1318f8737d1cf52f0931374cac6772f6634
IlyaMotorinMacOS <mike@magic.org>
2024-12-04 20:24:01 +0500

    Initial Commit

---
+++ MakeMeCoffeeApp/MakeMeCoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeMeCoffeeAppApp.swift
+//  MakeMeCoffeeApp
+//
+//  Created by Илья Моторин on 04.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MakeMeCoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MakeMeCoffeeAppTests/MakeMeCoffeeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MakeMeCoffeeAppTests.swift
+//  MakeMeCoffeeAppTests
+//
+//  Created by Илья Моторин on 04.12.2024.
+//
+
+import Testing
+@testable import MakeMeCoffeeApp
+
+struct MakeMeCoffeeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
